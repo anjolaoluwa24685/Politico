f6fe4c34ba5f1ee0ce9bd5506615d6a114b1dd2b
Anjolaoluwa Ebenezer <anjolaoluwaebenezer@gmail.com>
2019-04-15 18:59:49 +0100

    setting up database

---
+++ server/config/index.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_HOST, DB_PORT} = process.env
+const config = {
+    development: {
+        user: DB_USERNAME,
+        password: DB_PASSWORD,
+        database: DB_DATABASE,
+        host: DB_HOST,
+        port: DB_PORT
+
+    }
+}
+
+export default config;
